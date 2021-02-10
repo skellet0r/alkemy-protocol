bd5396703c53269a061b062b2e1b19d8fc5f9f48
Edward Amor <edward.amor3@gmail.com>
2021-02-10 14:32:38 -0500

    feat: add mock contract for testing cut

---
+++ contracts/mocks/MockContract.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.7.6;
+pragma abicoder v2;
+
+/// @dev Uses unstructured storage pattern
+contract MockContract {
+    function getter() external view returns (uint256 val_) {
+        bytes32 position = keccak256("diamond.standard.mock.contract.storage");
+        assembly {
+            val_ := sload(position)
+        }
+    }
+
+    function setter(uint256 val_) internal {
+        bytes32 position = keccak256("diamond.standard.mock.contract.storage");
+        assembly {
+            sstore(position, val_)
+        }
+    }
+}
